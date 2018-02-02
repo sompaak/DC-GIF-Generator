19047f3d76f553a75505f697cb86a5bb4da98501
Akhil <akhil.sompalli@snhu.edu>
2018-02-01 21:25:48 -0800

    Found more issues and added comments to debug the issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,73 +1,65 @@
- var movies = ["superman","Batman","Barry allen"];
+var movies = ["superman","Batman","Barry allen"];
 
-
-
-// Renders prexisting buttons with values in array. 
-//Also adds values to array if search button is clicked and renders new buttons with the corresponding values
-
+// takes prexisting values as well as new values gained from the user-input and renders them as buttons on the page 
 function renderButtons() 
 {
-        
-  $("#buttons").empty();       
- 
-  for (var i = 0; i < movies.length; i++) 
-  {
-    var myButton = $("<button>");
-    myButton.addClass("btn")      
-    myButton.attr("data-name", movies[i]);
-    myButton.text(movies[i]);
-    $("#buttons").append(myButton);
+    $("#buttons").empty();
+
+    for (var i = 0; i < movies.length; i++)
+     {
+      var myButton = $("<button>");
+      myButton.addClass("btn")
+      myButton.attr("data-name", movies[i]);
+      myButton.text(movies[i]);
+      $("#buttons").append(myButton);
+    }
 
     $("#add-movie").on("click", function(event) 
-      {
-          event.preventDefault();
-          var movie = $("#movie-input").val().trim();
-          movies.push(movie);
-          renderButtons();
-      });
-    }
-} 
-
-
-
-  //gets info from api and on the click of a button displays the gifs corresponding to the value the button contains
-
- function getinfo()
- {
-
-    var gif = $(this).attr("data-name");
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  gif + "&limit=10&api_key=dc6zaTOxFJmzC";
+    {
     
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      })
-      
-      .then(function(response) {
-        
+    event.preventDefault();
+    var movie = $("#movie-input").val().trim();
+    movies.push(movie);
 
-        console.log(response);
-         var results = response.data;
-         console.log(results)
+    renderButtons();
+  });
 
-        for (var i = 0; i < results.length; i++) {
-          var gif = $("<div>")
-          var GifImage = $("<img>");
-          GifImage.addClass("alignment")
-          GifImage.attr("src", results[i].images.fixed_height_small.url);  
-          gif.append(GifImage);
-          $("#gifs").prepend(gif);
-        }
-      
-     });
+}
+
+// gets Giphy API info and renders gifs on the the page
+
+function getinfo()
+{
+  var gif = $(this).attr("data-name");
+  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +  gif + "&limit=10&api_key=dc6zaTOxFJmzC";
+
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+
+  .then(function(response) 
+  {
+    console.log(response);
+    var results = response.data;
+    console.log(results)
+
+    for (var i = 0; i < results.length; i++) {
+      var gif = $("<div>")
+      var GifImage = $("<img>");
+      GifImage.addClass("alignment")
+      GifImage.attr("src", results[i].images.fixed_height_small.url);
+      gif.append(GifImage);
+      $("#gifs").prepend(gif);
+    }
+  
+  });
+
 }
 
 
-$(document).ready(function() { 
-
-  // calls functions
-  renderButtons()
-  $(document).on("click", ".btn", getinfo);
-      
-
-  });
+$(document).ready(function() 
+{  
+  renderButtons();
+  $(document).on("click", ".btn", getinfo); 
+});
